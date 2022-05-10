c1ab8f89d95d28bcbd0918ecf57cc22e8e189e7f
AbelSeMa <sexto_abel@hotmail.com>
2022-05-10 04:02:26 +0200

    pruebas

---
+++ principal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class principal {
+    public static void main(String[] args) {
+        int[] x = new int[5];
+        int res = 0;
+
+        System.out.println("Introduce cinco números: ");
+        for (int i = 0; i < x.length; i++) {
+            Scanner sc = new Scanner(System.in);
+            x[i] = sc.nextInt();
+        }
+
+        System.out.println();
+        for (int i = 0; i < x.length; i++) {
+            System.out.println(x[i]);
+            res += x[i];
+        }
+
+        System.out.println(res);
+    }
+
+}
